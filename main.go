a0017c6960a619c4fe69f441528e2ded70672d2c
Chris Rice <chrisrice93@gmail.com>
2017-11-11 13:01:41 -0700

    Fixed move rules, added move for up down and left

--- main.go
+++ main.go
@@ -6,26 +6,42 @@ import (
 	"os"
 
 	"github.com/donutmonger/2048/board"
+	"github.com/fatih/color"
 )
 
 func main() {
 	gameBoard := board.NewEmptyBoard()
 	gameBoard[0][3] = 2
-	gameBoard[0][1] = 2
 
 	scanner := bufio.NewScanner(os.Stdin)
 	var text string
 	for text != "q" { // break the loop if text == "q"
-		fmt.Println("")
+		gameBoard = board.PlaceRandomTwo(gameBoard)
 		fmt.Println(board.BoardToString(gameBoard))
 		fmt.Println("")
+
+		d := color.New(color.FgCyan, color.Bold)
+		d.Println("hello world")
 
 		fmt.Print("Enter your move: ")
 		scanner.Scan()
 		text = scanner.Text()
-		if text == "r" {
+
+		switch text {
+		case "r":
 			gameBoard = board.MoveRight(gameBoard)
+			break
+		case "l":
+			gameBoard = board.MoveLeft(gameBoard)
+			break
+		case "d":
+			gameBoard = board.MoveDown(gameBoard)
+			break
+		case "u":
+			gameBoard = board.MoveUp(gameBoard)
+			break
 		}
+
 	}
 
 }
