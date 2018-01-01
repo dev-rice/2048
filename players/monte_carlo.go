08ee614fcf6f16fc470bd88042b322e556c34992
Chris Rice <chrisrice93@gmail.com>
2018-01-01 14:48:24 -0700

    Parallelize monte carlo player a bit

--- players/monte_carlo.go
+++ players/monte_carlo.go
@@ -3,6 +3,8 @@ package players
 import (
 	"sort"
 
+	"sync"
+
 	"github.com/donutmonger/2048/actions"
 	"github.com/donutmonger/2048/board"
 	"github.com/donutmonger/2048/game"
@@ -42,27 +44,48 @@ func NewMonteCarloPlayer() MonteCarloPlayer {
 func (m MonteCarloPlayer) GetAction(b [][]int64) actions.Action {
 	a := make([]actionTuple, 0)
 
-	timesPerMove := 100
+	timesPerMove := 200
 
-	upBoard, upScore, err := board.MoveUp(b)
-	if err == nil {
-		a = append(a, actionTuple{action: actions.MoveUp, score: getAverageScoreForNRandomGamesForBoard(timesPerMove, upBoard) + float32(upScore)})
-	}
+	wg := sync.WaitGroup{}
+	wg.Add(4)
 
-	downBoard, downScore, err := board.MoveDown(b)
-	if err == nil {
-		a = append(a, actionTuple{action: actions.MoveDown, score: getAverageScoreForNRandomGamesForBoard(timesPerMove, downBoard) + float32(downScore)})
-	}
+	go func() {
+		upBoard, upScore, err := board.MoveUp(b)
+		if err == nil {
+			avgScore := getAverageScoreForNRandomGamesForBoard(timesPerMove, upBoard)
+			a = append(a, actionTuple{action: actions.MoveUp, score: avgScore + float32(upScore)})
+		}
+		wg.Done()
+	}()
 
-	leftBoard, leftScore, err := board.MoveLeft(b)
-	if err == nil {
-		a = append(a, actionTuple{action: actions.MoveLeft, score: getAverageScoreForNRandomGamesForBoard(timesPerMove, leftBoard) + float32(leftScore)})
-	}
+	go func() {
+		downBoard, downScore, err := board.MoveDown(b)
+		if err == nil {
+			avgScore := getAverageScoreForNRandomGamesForBoard(timesPerMove, downBoard)
+			a = append(a, actionTuple{action: actions.MoveDown, score: avgScore + float32(downScore)})
+		}
+		wg.Done()
+	}()
 
-	rightBoard, rightScore, err := board.MoveRight(b)
-	if err == nil {
-		a = append(a, actionTuple{action: actions.MoveRight, score: getAverageScoreForNRandomGamesForBoard(timesPerMove, rightBoard) + float32(rightScore)})
-	}
+	go func() {
+		leftBoard, leftScore, err := board.MoveLeft(b)
+		if err == nil {
+			avgScore := getAverageScoreForNRandomGamesForBoard(timesPerMove, leftBoard)
+			a = append(a, actionTuple{action: actions.MoveLeft, score: avgScore + float32(leftScore)})
+		}
+		wg.Done()
+	}()
+
+	go func() {
+		rightBoard, rightScore, err := board.MoveRight(b)
+		if err == nil {
+			avgScore := getAverageScoreForNRandomGamesForBoard(timesPerMove, rightBoard)
+			a = append(a, actionTuple{action: actions.MoveRight, score: avgScore + float32(rightScore)})
+		}
+		wg.Done()
+	}()
+
+	wg.Wait()
 
 	sort.Sort(actionTuples(a))
 	return a[len(a)-1].action
@@ -77,6 +100,5 @@ func getAverageScoreForNRandomGamesForBoard(n int, board [][]int64) float32 {
 		}
 		sum += float32(g.Play(NewRandomPlayer(), silentPrinter{}).Score)
 	}
-
 	return sum / float32(n)
 }
