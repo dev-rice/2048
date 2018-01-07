cc063b4d26e7e7a7a8de1ee7feb991ab8025244b
Chris Rice <chrisrice93@gmail.com>
2018-01-07 11:24:35 -0700

    uncommented stringer tests

--- board/stringer_test.go
+++ board/stringer_test.go
@@ -1,53 +1,53 @@
 package board
 
-//import (
-//	"testing"
-//
-//	"github.com/stretchr/testify/assert"
-//)
-//
-//func TestStringForEmptyBoard(t *testing.T) {
-//	expected :=
-//		`-----------------
-//|   |   |   |   |
-//-----------------
-//|   |   |   |   |
-//-----------------
-//|   |   |   |   |
-//-----------------
-//|   |   |   |   |
-//-----------------`
-//
-//	board := [][]int64{
-//		{0, 0, 0, 0},
-//		{0, 0, 0, 0},
-//		{0, 0, 0, 0},
-//		{0, 0, 0, 0},
-//	}
-//	s := NewStringer(board)
-//
-//	assert.Equal(t, expected, s.String())
-//}
-//
-//func TestStringForPopulatedBoard(t *testing.T) {
-//	expected :=
-//		`-------------------------
-//|     |     |     |   2 |
-//-------------------------
-//|     |     |   8 |   4 |
-//-------------------------
-//|     |  16 |  16 |  32 |
-//-------------------------
-//|     |  16 |  32 | 128 |
-//-------------------------`
-//
-//	board := [][]int64{
-//		{0, 0, 0, 2},
-//		{0, 0, 8, 4},
-//		{0, 16, 16, 32},
-//		{0, 16, 32, 128},
-//	}
-//	s := NewStringer(board)
-//
-//	assert.Equal(t, expected, s.String())
-//}
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestStringForEmptyBoard(t *testing.T) {
+	expected :=
+		`-----------------
+|   |   |   |   |
+-----------------
+|   |   |   |   |
+-----------------
+|   |   |   |   |
+-----------------
+|   |   |   |   |
+-----------------`
+
+	board := [][]int64{
+		{0, 0, 0, 0},
+		{0, 0, 0, 0},
+		{0, 0, 0, 0},
+		{0, 0, 0, 0},
+	}
+	s := NewStringer(board)
+
+	assert.Equal(t, expected, s.String())
+}
+
+func TestStringForPopulatedBoard(t *testing.T) {
+	expected :=
+		`-------------------------
+|     |     |     |   2 |
+-------------------------
+|     |     |   8 |   4 |
+-------------------------
+|     |  16 |  16 |  32 |
+-------------------------
+|     |  16 |  32 | 128 |
+-------------------------`
+
+	board := [][]int64{
+		{0, 0, 0, 2},
+		{0, 0, 8, 4},
+		{0, 16, 16, 32},
+		{0, 16, 32, 128},
+	}
+	s := NewStringer(board)
+
+	assert.Equal(t, expected, s.String())
+}
